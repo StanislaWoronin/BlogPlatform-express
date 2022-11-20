0eb6e673e7087d1d90efced10e46c01b134c9c56
Stanislav <stanislaworonin@gmail.com>
2022-11-20 15:30:00 +0300

    fix posts type in pagination

--- src/paginationContentPage.ts
+++ src/paginationContentPage.ts
@@ -1,12 +1,12 @@
 import {givePagesCount} from "./helperFunctions";
 import {BlogsConstructor} from "./types/blogs-constructor";
-import {PostsConstructor} from "./types/posts-constructor";
+import {PostViewModel} from "./types/posts-constructor";
 import {UsersType} from "./types/user-constructor";
 import {CommentViewModel} from "./types/comment-constructor";
 
 export const paginationContentPage = (pageNumber: string,
                                       pageSize: string,
-                                      content: BlogsConstructor | PostsConstructor | UsersType | CommentViewModel[],
+                                      content: BlogsConstructor | PostViewModel[] | UsersType | CommentViewModel[],
                                       totalCount: number) => {
 
     return {
